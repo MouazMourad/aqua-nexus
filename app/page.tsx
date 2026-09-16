5588a7ae8d7575d3aa705aed314f58dd7dcd4594
MouazMourad <150328052+MouazMourad@users.noreply.github.com>
2026-09-16 20:19:53 +0300

    Mount smart dashboard command layer

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
 "use client";
 
 import { AquaDashboard } from "@/components/dashboard/AquaDashboard";
+import { DashboardCommandLayer } from "@/components/dashboard/DashboardCommandLayer";
 
 export default function Home() {
-  return <AquaDashboard />;
+  return <><AquaDashboard/><DashboardCommandLayer/></>;
 }
