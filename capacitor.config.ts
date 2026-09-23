ad4530e9beb208d37aa906edb37d5c657bfab8e4
MouazMourad <150328052+MouazMourad@users.noreply.github.com>
2026-09-23 22:58:30 +0300

    Fix Capacitor 8 config type error

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,7 +4,6 @@ const config: CapacitorConfig = {
   appId: "com.aquanexus.app",
   appName: "Aqua Nexus",
   webDir: "out",
-  bundledWebRuntime: false,
   ios: {
     contentInset: "automatic"
   }
